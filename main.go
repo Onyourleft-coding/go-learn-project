ae53cff9aec9fb1d69b996b881a334c4de2abf88
林钟焱 <1577714120@qq.com>
2023-11-03 20:18:16 +0800

    init

---
+++ main.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"github.com/gin-gonic/gin"
+	"net/http"
+	"todo_list/bootstrap"
+	"todo_list/global"
+)
+
+func main() {
+
+	//初始化配置
+	bootstrap.InitializeConfig()
+
+	//初始化日志
+	global.App.Log = bootstrap.InitializeLog()
+	global.App.Log.Info("log int success!")
+
+	//初始化数据库
+	global.App.DB = bootstrap.InitializeDB()
+	//程序关闭前，释放数据库连接
+	defer func() {
+		if global.App.DB != nil {
+			db, _ := global.App.DB.DB()
+			db.Close()
+		}
+	}()
+
+	r := gin.Default()
+	//	测试路由
+	r.GET("/ping", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+			"data":    "pong",
+			"code":    0,
+			"message": "请求成功",
+		})
+	})
+	r.Run(":" + global.App.Config.App.Port)
+}
